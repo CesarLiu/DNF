be4449aece1a5bb4d0698e4cb1607e4229cb8c4d
Xiang Liu <44816474+CesarLiu@users.noreply.github.com>
2025-12-23 23:53:38 +0100

    Update __init__.py to remove unused tracker
    Removed PETRTrackerDQ_objectlist_wdnq from exports.

--- clf-dqtrack/projects/mmdet3d_plugin/models/trackers/__init__.py
+++ clf-dqtrack/projects/mmdet3d_plugin/models/trackers/__init__.py
@@ -4,10 +4,9 @@ from .petr_tracker_dq import PETRTrackerDQ
 from .detr3d_tracker_dq import DETR3DTrackerDQ
 
 from .petr_tracker_dq_objectlist import PETRTrackerDQ_objectlist
-from .petr_tracker_dq_objectlist_concate import PETRTrackerDQ_objectlist_wdnq
 from .petr_tracker_dq_objectlist_dnq_static import PETRTrackerDQ_objectlist_dnq_static
 
 __all__ = ['UVTRTrackerDQ', 'StereoTrackerDQ', 
            'PETRTrackerDQ', 'DETR3DTrackerDQ',
-           'PETRTrackerDQ_objectlist','PETRTrackerDQ_objectlist_wdnq',
+           'PETRTrackerDQ_objectlist',
            'PETRTrackerDQ_objectlist_dnq_static']
